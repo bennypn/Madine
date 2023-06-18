0b7110587aa33dda1d2de0a00c832ee069b8fdf0
bennypn <bennyperdana.jakarta@gmail.com>
2023-06-18 12:32:08 +0700

    build dif

--- app/src/main/java/com/example/madine/SplashScreen.java
+++ app/src/main/java/com/example/madine/SplashScreen.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -13,6 +14,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         setContentView(R.layout.activity_splash_screen);
 
         new Handler().postDelayed(new Runnable() {
